158023c2b1767480165870101c5b0a7cbbbc257f
gnli <nuaalgn@163.com>
2025-03-02 19:31:11 +0800

    update

--- extract_sc_replayed_speech.py
+++ extract_sc_replayed_speech.py
@@ -2,38 +2,36 @@
 import librosa
 import librosa.display
 import soundfile
+import os
 
 
 def extract_single_channel_wav(fpath, fname):
     y, sr = librosa.load(f"{fpath}/{fname}.wav", sr=16000, mono=False)
-    # soundfile.write(f"{fpath}/clean/{fname}.wav", y[15, :], sr)
-    # soundfile.write(f"{fpath}/rvb-only/{fname}.wav", y[16, :], sr)
+    if not os.path.exists(f"{fpath}/rvb-only"):
+        os.makedirs(f"{fpath}/mixture")
+        os.makedirs(f"{fpath}/clean")
+        os.makedirs(f"{fpath}/rvb-only")
+    soundfile.write(f"{fpath}/clean/{fname}.wav", y[15, :], sr)
+    soundfile.write(f"{fpath}/rvb-only/{fname}.wav", y[16, :], sr)
     soundfile.write(f"{fpath}/mixture/{fname}.wav", y[0, :], sr)
 
 
-path = "replayed/0-15/0-mc"
-name = "6352498867394358158-00019"
+path = "mixture_clean/eval"
+name = "6343252661930009508-00020"
 extract_single_channel_wav(path, name)
 
-path = "replayed/15-45/0-mc"
-name = "6352893145392128503-00036"
+path = "mixture_clean/eval"
+name = "6354369755148493407-00048"
 extract_single_channel_wav(path, name)
 
-path = "replayed/45-90/0-mc"
-name = "6392525815109495392-00008"
+path = "mixture_clean/eval"
+name = "6364327207327548190-00036"
 extract_single_channel_wav(path, name)
 
+path = "mixture_clean/eval"
+name = "6367813861908922495-00004"
+extract_single_channel_wav(path, name)
 
-
-# replayed
-# 6392525815109495392-00008 female angle_difference=90  WER=0
-# /project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6392525815109495392-00008.wav
-# {'lip_path': ['/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6392525815109495392/00008.npz', '/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6392525815109495392/00008.npz'], 'lipemb_path': ['/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/lip_emb_data/test/emb/6392525815109495392/00008.npy'], 'wav_path': '/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6392525815109495392-00008.wav', 'n_spk': 2, 'spk_id': ['6392525815109495392', '6343608285091658983'], 'spk_doa': ['150', '60'], 'time_idx': [[0.0, 1.792]]}
-
-# 6352893145392128503-00036 female angle_difference=30 WER=1
-# /project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6352893145392128503-00036.wav
-# {'lip_path': ['/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6352893145392128503/00036.npz', '/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6352893145392128503/00036.npz'], 'lipemb_path': ['/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/lip_emb_data/test/emb/6352893145392128503/00036.npy'], 'wav_path': '/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6352893145392128503-00036.wav', 'n_spk': 2, 'spk_id': ['6352893145392128503', '6343608285091658983'], 'spk_doa': ['30', '60'], 'time_idx': [[0.0, 2.112]]}
-
-# 6352498867394358158-00019 male angle_difference=15 WER=0
-# /project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6352498867394358158-00019.wav
-# {'lip_path': ['/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6352498867394358158/00019.npz', '/CAdisk/tomasyu/LRS2_image/LRS2/data/main/6352498867394358158/00019.npz'], 'lipemb_path': ['/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/lip_emb_data/test/emb/6352498867394358158/00019.npy'], 'wav_path': '/project_bdda6/bdda/gnli/projects/LRS2/data_prepare/wav_data/replay/record_out/6352498867394358158-00019.wav', 'n_spk': 2, 'spk_id': ['6352498867394358158', '6371470597064737331'], 'spk_doa': ['15', '30'], 'time_idx': [[0.0, 1.408]]}
+path = "mixture_clean/eval"
+name = "6368177216011755289-00003"
+extract_single_channel_wav(path, name)
--- extract_sc_simulated_speech.py
+++ extract_sc_simulated_speech.py
@@ -2,18 +2,51 @@
 import librosa
 import librosa.display
 import soundfile
+import os
 
 
 def extract_single_channel_wav(fpath, fname):
     y, sr = librosa.load(f"{fpath}/{fname}.wav", sr=16000, mono=False)
+    if not os.path.exists(f"{fpath}/clean"):
+        os.makedirs(f"{fpath}/clean")
+        os.makedirs(f"{fpath}/rvb-only")
+        os.makedirs(f"{fpath}/mixture")
     soundfile.write(f"{fpath}/clean/{fname}.wav", y[15, :], sr)
     soundfile.write(f"{fpath}/rvb-only/{fname}.wav", y[16, :], sr)
     soundfile.write(f"{fpath}/mixture/{fname}.wav", y[0, :], sr)
 
 
-# path = "simulated/0-15/0-mc"
-# name = "00019"
+# dataset = "ECAPA"
+# path = f"mixture_clean/dev/{dataset}"
+# name = "LRS3XX-02221-TEDXXXXX-XXXXXX-ih_10000046_0000000_0000457"
 # extract_single_channel_wav(path, name)
+#
+# dataset = "ECAPA"
+# path = f"mixture_clean/dev/{dataset}"
+# name = "LRS3XX-01096-TEDXXXXX-XXXXXX-ih_10000015_0000223_0000597"
+# extract_single_channel_wav(path, name)
+#
+# dataset = "ECAPA"
+# path = f"mixture_clean/dev/{dataset}"
+# name = "LRS3XX-01344-TEDXXXXX-XXXXXX-ih_10000021_0000000_0000298"
+# extract_single_channel_wav(path, name)
+
+
+dataset = "xvector"
+path = f"mixture_clean/dev/{dataset}"
+name = "LRS3XX-00203-TEDXXXXX-XXXXXX-ih_10000018_0000001_0000349"
+extract_single_channel_wav(path, name)
+
+dataset = "xvector"
+path = f"mixture_clean/dev/{dataset}"
+name = "LRS3XX-01903-TEDXXXXX-XXXXXX-ih_10000015_0000002_0000605"
+extract_single_channel_wav(path, name)
+
+dataset = "xvector"
+path = f"mixture_clean/dev/{dataset}"
+name = "LRS3XX-01060-TEDXXXXX-XXXXXX-ih_10000002_0000466_0000861"
+extract_single_channel_wav(path, name)
+
 #
 # path = "simulated/15-45/0-mc"
 # name = "00027"
